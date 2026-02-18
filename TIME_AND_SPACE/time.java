10146f10c2b63246e1a59e67438fb4a99a24d453
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-02-18 20:16:48 +0530

    time and space complexity

---
+++ TIME_AND_SPACE/time.java
@@ -0,0 +1,10 @@
+public class time {
+    public static void main(String[] args) {
+        long s = System.currentTimeMillis();
+        for(int i=0; i<1000000000; i++){
+            
+        }
+        long l = System.currentTimeMillis();
+        System.out.println(l-s);
+    }
+}
