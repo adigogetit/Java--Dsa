80e789be7411e6c19fe72f2a5e9fdabf33470bc4
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-03-16 23:36:19 +0530

    changes in code

--- RECURSION/recursion_questions/1/subsequesnces.java
+++ RECURSION/recursion_questions/1/subsequesnces.java
@@ -2,24 +2,22 @@ public class subsequesnces {
     public static void main(String[] args) {
         String ques = "abc";
         // print(ques,"");
-        System.out.print("\n" + print(ques,""));
+        print(ques,"");
 
         System.out.println();
         // using for loop
         solve(0,ques,"");
     }
-    // count solutions
-    public static int print(String ques, String ans){
+    public static void print(String ques, String ans){
         if(ques.length()==0){
             System.out.print(ans+" ");
-            return 1;
+            return;
         }
         char ch = ques.charAt(0);
         // not pick
-        int a1 = print(ques.substring(1),ans);
+        print(ques.substring(1),ans);
         // pick
-        int a2 = print(ques.substring(1),ans+ch);
-        return a1+a2;
+        print(ques.substring(1),ans+ch);
     }
 
     public static void solve(int start, String ques, String ans) {
