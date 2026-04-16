aee037642cb75b7c06410453398704b8a026cfb7
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-04-16 12:01:28 +0530

    Queue using array

--- QUEUE/bacis/Queue_using_array.java
+++ QUEUE/bacis/Queue_using_array.java
@@ -11,42 +11,69 @@ class SimpleQueue {
         rear = -1;
     }
 
-    public void enqueue(int x) {
+    public boolean isEmpty() { // O(1)
+        return rear == -1;
+    }
+
+    public void enqueue(int x) { // add O(1)
         if (rear == capacity - 1) {
             System.out.println("Queue is Full");
             return;
         }
-        arr[++rear] = x;
+        rear = rear + 1;
+        arr[rear] = x;
     }
 
-    public int dequeue() {
+    public int dequeue() { // remove O(1)
         if (front > rear) {
             System.out.println("Queue is Empty");
             return -1;
         }
-        return arr[front++];
+        int val = arr[front];
+        front = front + 1;
+        return val;
     }
 
-    public int peek() {
+    public int peek() { // see O(1)
         if (front > rear) return -1;
         return arr[front];
     }
+
+    public void display() {
+        if (front > rear) {
+            System.out.println("Queue is Empty");
+            return;
+        }
+
+        for (int i = front; i <= rear; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
 }
 
-// Separate class 
+// Separate class
 public class Queue_using_array {
     public static void main(String[] args) {
 
         SimpleQueue q = new SimpleQueue(5);
 
+        System.out.println(q.isEmpty());
         q.enqueue(10);
         q.enqueue(20);
         q.enqueue(30);
+        System.out.println(q.isEmpty());
 
         System.out.println(q.dequeue()); // 10
-        System.out.println(q.peek());    // 20
+        System.out.println(q.peek()); // 20
 
+        q.display();
+        
         q.enqueue(40);
         q.enqueue(50);
+        System.out.println(q.dequeue());
+
+        q.display();
+        
     }
 }
