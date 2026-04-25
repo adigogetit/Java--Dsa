1eba15cbbf525483bc9bdbf2db49a74d6f5474e4
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-04-25 23:10:34 +0530

    reverse ll using interative method

---
+++ LINKED_LIST/question/reverse_ll.java
@@ -0,0 +1,75 @@
+package questions;
+
+public class reverse_ll {
+    public static class Node {
+        public int data;
+        public Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    public void printList() {
+        if (head == null) {
+            System.out.println("Linked list is empty");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public void reverseIterative() {
+        Node prev = null;
+        Node current = head;
+        tail = head;
+
+        while (current != null) {
+            Node nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+
+        head = prev;
+    }
+
+
+    public static void main(String[] args) {
+        reverse_ll list = new reverse_ll();
+
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.addLast(40);
+        list.addLast(50);
+
+        System.out.println("Original list:");
+        list.printList();
+
+        list.reverseIterative();
+        System.out.println("After iterative reverse:");
+        list.printList();
+
+    }
+}
