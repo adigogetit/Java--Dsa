df9ed4de7d2ed6bd75dfe1369fc18ecd921821f6
Aditya Kumar Singh <adityasingh12ka36@gmail.com>
2026-07-24 10:30:00 +0530

    Subtree of another tree

--- Tree/Basics/diameter_subtree.java
+++ Tree/Basics/diameter_subtree.java
@@ -71,7 +71,29 @@ public class diameter_subtree {
         return new Info(dia, ht);
     }
 
+    // Subtree in tree
+    public static boolean isSubtree(Node root, Node subRoot) {
+        if(root == null)
+            return false;
 
+        if(root.data == subRoot.data) {
+            if(isIdentical(root, subRoot)) {
+                return true;
+            }
+        }
+        boolean leftAns = isSubtree(root.left, subRoot);
+        boolean rightAns = isSubtree(root.right, subRoot);
+
+        return leftAns || rightAns;
+    }
+
+    public static boolean isIdentical(Node root, Node subRoot) {
+        if(root == null && subRoot == null)return true;
+        if(root == null || subRoot == null)return false;
+        if(root.data != subRoot.data)return false;
+
+        return isIdentical(root.left, subRoot.left) && isIdentical(root.right, subRoot.right);
+    }
 
     public static void main(String[] args) {
 
