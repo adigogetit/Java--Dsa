9ec882bb05ad21660095e3ee79adb4423322402b
Aditya Kumar Singh <adityasingh12ka36@gmail.com>
2026-08-10 22:56:01 +0530

    Kth level nodes of a tree

---
+++ Tree/Tree Questions/kth_level.java
@@ -0,0 +1,42 @@
+public class kth_level {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        // constructer
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static void kth(Node root, int level, int k){
+        if(root == null)return;
+        if(level == k){
+            System.out.print(root.data+" ");
+            return;
+        }
+
+        kth(root.left, level+1, k);
+        kth(root.right, level+1, k);
+
+    }
+
+    public static void main(String[] args) {
+
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        int k = 3;
+        int level = 1;
+        kth(root, level, k);
+
+    }
+}
